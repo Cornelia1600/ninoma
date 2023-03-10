226968138f2a4bd3f6721d4fed196dbf99343390
nikkimz <azimi.niki@gmail.com>
2023-03-10 23:40:34 +0100

    Update DirecteurVue.php

--- vues/DirecteurVue.php
+++ vues/DirecteurVue.php
@@ -4,38 +4,11 @@ function afficherPageDirecteur(){
     $form = '
 	<form id="directeur_form" action="#" method="post">
         <fieldset> 
-            <legend> Page de Directeur </legend>
+        <legend> Création, Suppression ou Modification </legend>
     <p>
         <label for="acces">Login ou MDP</label>		
         <input type="submit" Value="Créer Acces" name="creation_acces"/>
         <input type="submit" Value="Modifier Acces" name="modif_acces"/>
-    </p>
-    <p>
-        <label for="acces">Rendez-vous</label>		
-        <input type="submit" Value="Les élements de rdv" name="modif_rdv"/>
-    </p>
-    <p>
-        <label for="acces">Medecin</label>		
-        <input type="submit" Value="Créer" name="modif_medecin"/>
-        <input type="submit" Value="Supprimer" name="modif_medecin"/>
-    </p>
-  
-    </fieldset>
-    </form>';
-
-    return $form;
-}
-
-function afficherModificationAcces(){
-    $form = '
-
-	<form id="directeur_form" action="#" method="post">
-        <fieldset> 
-        <legend> Création, Suppression ou Modification </legend>
-    <p>
-        <label for="acces">Login ou MDP</label>		
-        <input type="submit" Value="Modifier Acces" name="modif_acces"/>
-        <input type="submit" Value="Créer Acces" name="create_acces"/>
     </p>
     <p>
         <label for="acces">Rendez-vous</label>		
@@ -52,6 +25,13 @@ function afficherModificationAcces(){
     </fieldset>
     </form>';
 
+    return $form;
+}
+
+function afficherModificationAcces(){
+    $form = '
+
+	'
     return $form;
 
 
