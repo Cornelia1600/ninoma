e35955244896a3539d8e96906376ea2687590cda
Cornelia1600 <123503778+Cornelia1600@users.noreply.github.com>
2023-03-10 14:41:29 +0100

    Modifs

--- modeles/RdvModele.php
+++ modeles/RdvModele.php
@@ -2,8 +2,9 @@
     function getRdvByDate($idmedecin, $date, $heure){
         $connexion=getConnect();
 
-        $dateTime = new DateTime($date);
-        $dateTime->setTime($heure, 0);
+        // Transforme date et heure en datetime
+        $dateTime = new DateTime($date); // Crée un dateTime à la date du rdv 
+        $dateTime->setTime($heure, 0);// On modifie l'heure du dateTime à celle du rdv
 
         $requeteRdv='SELECT * FROM rdv WHERE IDPERS="'. $idmedecin. '" AND DATERDV="' . $dateTime->format('Y-m-d H:i:s'). '"';
         $resultatRdv=$connexion->query($requeteRdv); 
--- vues/AgentVue.php
+++ vues/AgentVue.php
@@ -1,33 +1,65 @@
 <?php
 
-function afficherFormPriseRdv($specialites, $medecins = []){
-    $contenu = '<form method="POST"><fieldset><legend>Prise de rendez-vous</legend>
-        <div>
+function afficherFormPriseRdv($specialites, $medecins = [], $rdvDejaPris = false, $motifs = []){
+    $contenu = '<form method="POST"><fieldset><legend>Prise de rendez-vous</legend>';
+        if ($rdvDejaPris) {
+            $contenu.= '<h3 style="color:red;">RDV déjà pris. Choisissez une autre date</h3>';
+        }
+        $contenu.='<div>
             <label for="specialite">Choisir une spécialité : </label>
-            <select name="specialite" id="specialite">
+            <select name="specialite" id="specialite"> 
                 <option value="" selected disabled hidden>-</option>
             ';
+            // On affiche une liste déroulante. 
              
-            for ($i=0; $i < count($specialites) ; $i++) {
-                
-                $contenu.='<option value="' . $specialites[$i]->IDSP .'"';
+            for ($i=0; $i < count($specialites) ; $i++) { 
+                // Pour chaque spécialité, on crée une option dans la liste
+                $contenu.='<option value="' . $specialites[$i]->IDSP .'"';// la valeur renvoyée = IDSPE
                 if(isset($_POST["specialite"]) && $_POST["specialite"] == $specialites[$i]->IDSP) {
-                    $contenu.=' selected="selected"';
+                    $contenu.=' selected="selected"'; 
                 }
-                $contenu.='>'. $specialites[$i]->LIBELLESP .'</option>';
+                $contenu.='>'. $specialites[$i]->LIBELLESP .'</option>'; // La valeur à afficher = libellé
             }
         
     $contenu.= '</select></div>';
 
-    if (count($medecins) > 0) {
+    if (count($medecins) > 0) {// si on a récupéré des médecins d'une spécialité / count renvoi la taille du tableau
         $contenu.='<div>
         <label for="medecin">Choisir un médecin : </label>
         <select name="medecin" id="medecin">
             <option value="" selected disabled hidden>-</option>
         ';
          
-        for ($i=0; $i < count($medecins) ; $i++) { 
-            $contenu.='<option value="' . $medecins[$i]->IDPERS .'">'. $medecins[$i]->PRENOM . ' ' . $medecins[$i]->NOM .'</option>';
+        for ($i=0; $i < count($medecins) ; $i++) {
+            $contenu .= '<option value="' . $medecins[$i]->IDPERS .'"'; // la valeur renvoyée = IDSPE 
+
+            if (isset($_POST["medecin"]) && $_POST["medecin"] == $medecins[$i]->IDPERS) {
+                $contenu .= ' selected="selected"';
+            }
+            $contenu .= '>' . $medecins[$i]->PRENOM . ' ' . $medecins[$i]->NOM . '</option>'; 
+
+            /* Explication :
+                <option value="452342" selected="selected">Mon option</option>
+                
+                <option value=" | fixe
+
+                452342 | $medecins[$i]->IDPERS
+                
+                " | fixe
+
+                selected="selected" | depend de isset($_POST["medecin"]) && $_POST["medecin"] == $medecins[$i]->IDPERS
+                 
+                > | fixe
+                
+                Mon | prenom
+                
+                espace | fixe
+                
+                option | nom
+                
+                </option> | fixe
+
+            */
         }
         
     
@@ -47,12 +79,69 @@ function afficherFormPriseRdv($specialites, $medecins = []){
 
     }
 
+    if(count($motifs) > 0){ // Si il existe un motif, alors afficher (avec le select) //TODO
+        $contenu.='<div>
+        <label for="motif">Choisir un motif : </label>
+        <select name="motif" id="motif">
+            <option value="" selected disabled hidden>-</option>
+        ';
+         
+        for ($i=0; $i < count($motifs) ; $i++) {
+            $contenu .= '<option value="' . $motifs[$i]->IDMO .'"'; // la valeur renvoyée = IDMO
+            if (isset($_POST["motif"]) && $_POST["motif"] == $motifs[$i]->IDMO) {
+                $contenu .= ' selected="selected"';
+            }
+            $contenu .= '>' . $motifs[$i]->LIBELLEMO . ' - ' . $motifs[$i]->PRIXMO . '€</option>'; 
+        }
 
+        $contenu.= '</select></div>';
+
+    }
 
     $contenu.='
             <button type="submit" name="prise_rdv">Continuer</button>
     </fieldset></form>';
+    // --> ajouter les valeurs des champs du formulaire dans la supervariable $_POST à la clé du nom du champ 
+    // ==> $_POST["name_input"] = "value_input"
+    // => + recharge la page
 
+    return $contenu;
+}
+
+/* A afficher 
+Le nom du patient // TODO après
+Le nom du médecin avec sa spécialité
+La date et l'heure du rdv
+Le motif 
+Les consignes
+Les pièces à fournir
+Le prix
+
++ un btn "valider le rdv"
+*/
+function afficherRecapRdv($specialite, $medecin){//TODO concatener le rdv
+    $contenu = '<form method="POST">
+        <h2>Récap du rdv<h2>
+        <div>
+            <p>Nom du médecin :'
+            . $medecin->PRENOM . ' ' . $medecin->NOM.
+            '
+
+            <p>
+            
+
+            <p>Consignes : // For pour afficher chaque élément de la liste
+                <ul>
+                    <li></li>// avec affichage dynamique
+                    <li></li>
+                </ul>
+            <p>
+
+
+    ';
+
+
+    $contenu.='</form>';
     return $contenu;
 }
 
